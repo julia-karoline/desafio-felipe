efa6759f69737d47fb988da361cb26587d1af2a0
julia-karoline <julliakkaroline@gmail.com>
2024-05-01 17:59:33 -0300

    desafio terminado

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-asas
+let nomeHeroi = "Ana"
+let xp = 9001
+
+if (xp <= 1000) {
+    nivel = "Ferro"
+}
+if (xp >= 1001) {
+    nivel = "Bronze"
+}
+if (xp >= 2001) {
+    nivel = "Prata"
+}
+if (xp >= 5001) {
+    nivel = "Ouro"
+}
+if (xp >= 7001) {
+    nivel = "Platina"
+}
+if (xp >= 8001) {
+    nivel = "Ascendente"
+}
+if (xp >= 9001) {
+    nivel = "Imortal"
+}
+if (xp >= 10001) {
+    nivel = "Radiante"
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel);
